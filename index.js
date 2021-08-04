c6cf20b4d8acbee0842fd40e3f8cb4b03add0798
ValentinNM <57604614+ValentinNM@users.noreply.github.com>
2021-08-04 20:12:10 +0300

    search by state done

--- index.js
+++ index.js
@@ -9,10 +9,37 @@ let state = {
   }
 };
 
+const formEl = document.querySelector ('#select-state-form');
+console.log("inside FormEl:", formEl);
+
+formEl.addEventListener("submit", (event) => {
+  console.log("click is working");
+  event.preventDefault();
+
+  const stateInput = formEl.querySelector("#select-state");
+  console.log("stateInput: ", stateInput.value);
+  
+  const url = `https://api.openbrewerydb.org/breweries?by_state=${stateInput.value}`;
+  
+  fetch(url) 
+  .then ((res) => res.json())
+  .then((beerState) => {
+    console.log("inside FETCH beerState: ", beerState);
+  
+  });
+ formEl.reset();
+
+} )
+
+
 function renderBreweriesList() {
   console.log("Inside renderBreweriesList: ", state.breweries);
 
-  for (let i = 0; i < state.breweries.length; i++) {}
+
+  for (let i = 0; i < state.breweries.length; i++) {
+    stateBrew = state.breweries[i];
+    console.log("stateBrew: ", stateBrew);
+  }
 }
 
 renderBreweriesList();
